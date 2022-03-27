b56ebb3ab7dc5fca94b2f82987d9de1d110a1af8
Josue Alfonzo <16-00436@usb.ve>
2022-03-27 14:24:21 -0400

    Actualizacion de salarios

---
+++ salarios.js
@@ -0,0 +1,102 @@
+const colombia = [];
+colombia.push({
+    name : "Juan",
+    salary : 500,
+});
+
+colombia.push({
+    name : "Daniel", 
+    salary : 850
+});
+
+colombia.push({
+    name : "Freddy", 
+    salary : 500
+});
+
+colombia.push({
+    name : "Marlin", 
+    salary : 360
+});
+
+colombia.push({
+    name : "Jesus", 
+    salary : 420
+});
+
+colombia.push({
+    name : "Serena" , 
+    salary : 460
+});
+
+
+colombia.push({
+    name : "Barbara", 
+    salary : 150
+});
+
+colombia.push({
+    name : "Orlando", 
+    salary : 320
+});
+
+colombia.push({
+    name : "Josue", 
+    salary : 650
+});
+
+colombia.push({
+    name : "Paola", 
+    salary : 2400
+});
+
+colombia.push({
+    name : "Gonzalo", 
+    salary : 100
+});
+
+colombia.push({
+    name : "Jeacquelin", 
+    salary : 500
+});
+
+colombia.push({
+    name : "Enmanuel", 
+    salary : 700
+});
+
+colombia.push({
+    name : "Karelis",
+    salary : 850
+});
+
+colombia.push({
+    name : "Gabriel", 
+    salary : 1500
+});
+
+colombia.push({
+    name : "Jose", 
+    salary : 200
+});
+
+colombia.push({
+    name : "Maria", 
+    salary : 700
+});
+
+colombia.push({
+    name : "Roberto", 
+    salary : 4600
+});
+
+colombia.push({
+    name : "Roberto", 
+    salary : 4600
+});
+
+colombia.push({
+    name : "Roberto", 
+    salary : 5000
+});
+
